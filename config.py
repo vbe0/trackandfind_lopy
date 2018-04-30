2b931704f1ecfd41c86d21e2ddcc5b7894fcbdfd
Valter Berg <vbe013@post.uit.no>
2018-04-30 12:26:52 +0200

    greit

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ dev_eui = "ffffffff00001323"
 app_eui = "8000000000000006"
 app_key = "30f8d03f2b9e8ea1471743fd245d4f85"
 
+dev_eui2 = "ffffffff00001413"
+app_key2 = "7e8de5dbb25b100b23c1cf94af1d6530"
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from config import dev_eui, app_eui, app_key, dummyThings
+from config import dev_eui, app_eui, app_key, dev_eui2, app_key2
 from lora import LORA
 
 from led import LED
@@ -9,14 +9,18 @@ from L76GNSS import L76GNSS # gps
 from time import sleep
 
 def setup():
-    global n, gps, sleep_time, dn, py
+    global n, gps, sleep_time, dn, py, is_n
     
+    is_n = True
     # Initial sleep time
-    sleep_time = 30
+    sleep_time = 15
 
     # Connect to LoRaWAN Decent
     n = LORA()
     n.connect(dev_eui, app_eui, app_key)
+
+    n2 = LORA()
+    n.connect(dev_eui2, app_eui, app_key2)
     
 
     py = Pytrack()
@@ -26,12 +30,22 @@ def setup():
     # Connect Sensors
     print("Setup... done")
 
+def sendData(m_lat, m_lng, battery): 
+    global is_n
+    if (is_n):
+        data = "%s %s %s" % (m_lat, m_lng, battery)
+        n.send(data)
+        is_n = False
+    else:
+        lat = m_lat - 69.737993 + m_lat
+        lng = m_lng - 18.812860 + m_lat
+        data = "%s %s %s" % (lat, lng, battery)
+        n2.send(data)
+        is_n = True
 
 if __name__ == "__main__":
     # Setup network & sensors
     setup()
-    #LED.heartbeat(False)
-    #LED.off()
 
     while True:
         sleep(sleep_time)
@@ -44,7 +58,7 @@ if __name__ == "__main__":
             m_lat, m_lng = gps.coordinates()
             battery = '{}V'.format(py.read_battery_voltage())
 
-            data = "%s %s %s" % (m_lat, m_lng, battery)
+            #data = "%s %s %s" % (m_lat, m_lng, battery)
             #print("Data: ", data)
         except Exception as e:
             print("Measure error: ", e)
@@ -55,6 +69,7 @@ if __name__ == "__main__":
         else :
             # Send packet
             LED.blink(2, 0.1, 0xf0f000)        
-            response = n.send(data)
+            #response = n.send(data)
+            sendData(m_lat, m_lng, battery)
 
 
