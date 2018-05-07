d7de24d964ce3ba0eb63ca85f5b26a26656a4028
Valter Berg <vbe013@post.uit.no>
2018-05-07 13:55:39 +0200

    Cool shit prad pit

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def setup():
         py.go_to_sleep(gps=True)
 
     
-    gps = L76GNSS(py, timeout=30)
+    gps = L76GNSS(py, timeout=5)
 
     acc = LIS2HH12(py)
     # Connect Sensors
@@ -37,50 +37,54 @@ if __name__ == "__main__":
     # Setup network & sensors
 
     setup()
-    data = ""
-    m_lat = m_lng = None
-    # Measure
-    try:
-        sleep(1)
-        m_lat, m_lng = gps.coordinates()
-        print ("GPS: %s, %s " % (m_lat, m_lng))
-        sleep(1)
-        x, y, z = acc.acceleration()
-        print ("Acc: x: %s, y: %s, z: %s" % (x, y, z))
-        x_1, y_1, z_1 = "%.2f" % x, "%.2f" % y, "%.2f" % z 
-        sumAcc = "%.2f" % (float(x_1) + float(y_1) + float(z_1))
-        print ("Sum x, y, z Acc: %s" % (sumAcc))
 
-        battery = py.read_battery_voltage()
-        battery = py.read_battery_voltage()
-        print("Battery: ", battery)
-        battery  = "%.2f" % float(battery) 
-        
-        # Don't hava an temperature sensor, so just sets an value 
-        temp = 20.5
-        
-        data = "%s %s %s %s %s" % (m_lat, m_lng, battery, temp, sumAcc)
-        print("Data: ", data, "Size:", len(data))
-    except Exception as e:
-        print("Measure error: ", e)
+    while (True):
+        sleep(5
+        )
+        data = ""
+        m_lat = m_lng = None
+        # Measure
+        try:
+            sleep(1)
+            m_lat, m_lng = gps.coordinates()
+            print ("GPS: %s, %s " % (m_lat, m_lng))
+            sleep(1)
+            x, y, z = acc.acceleration()
+            print ("Acc: x: %s, y: %s, z: %s" % (x, y, z))
+            x_1, y_1, z_1 = "%.2f" % x, "%.2f" % y, "%.2f" % z 
+            sumAcc = "%.2f" % (float(x_1) + float(y_1) + float(z_1))
+            print ("Sum x, y, z Acc: %s" % (sumAcc))
 
-    #print('Coords:', "{},{}".format(m_lat, m_lng))
-    if m_lat == None: 
-        print("Failed to receive gps signals")
-        LED.blink(1, 0.1, 0x0000ff)
-        LED.off()  
-    else :
-        # Send packe
-        LED.blink(2, 0.1, 0xf0f000)
-        LED.off()    
-    try:
-        response = n.send(data)
-    except:
-        print("Failed to send data, should try to reconnect")
-        n.eraseloraSaved()
+            battery = py.read_battery_voltage()
+            battery = py.read_battery_voltage()
+            print("Battery: ", battery)
+            battery  = "%.2f" % float(battery) 
+            
+            # Don't hava an temperature sensor, so just sets an value 
+            temp = 20.5
+            
+            data = "%s %s %s %s %s" % (m_lat, m_lng, battery, temp, sumAcc)
+            print("Data: ", data, "Size:", len(data))
+        except Exception as e:
+            print("Measure error: ", e)
+
+        #print('Coords:', "{},{}".format(m_lat, m_lng))
+        if m_lat == None: 
+            print("Failed to receive gps signals")
+            LED.blink(1, 0.1, 0x0000ff)
+            LED.off()  
+        else :
+            # Send packe
+            LED.blink(2, 0.1, 0xf0f000)
+            LED.off()    
+        try:
+            response = n.send(data)
+        except:
+            print("Failed to send data, should try to reconnect")
+            n.eraseloraSaved()
 
 
     #Go to deep sleep 
-    py.setup_sleep(sleep_time)
-    py.go_to_sleep(gps=True)
+    # py.setup_sleep(sleep_time)
+    # py.go_to_sleep(gps=True)
 
